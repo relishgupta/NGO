5813df2f2b2349ac96508d9a5ed624225bb24aae
relish <guptarelish333@gmail.com>
2023-05-13 07:30:15 +0530

    changed projects to services

---
+++ src/components/services.jsx
@@ -0,0 +1,11 @@
+const Services = () => {
+  return (
+    <div name='services' className="pt-20 h-screen w-full bg-gra">
+      <div className="pl-5 text-6xl text-blue-800">
+        Services
+      </div>
+    </div>
+  )
+}
+
+export default Services;
