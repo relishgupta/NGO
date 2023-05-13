71dd4efeb91f9c92bc6bb77a352927558f007c85
relish <guptarelish333@gmail.com>
2023-05-13 06:02:46 +0530

    added award.png

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,11 +8,11 @@ const Home = () => {
     <div name='home' className="flex flex-col md:flex pt-20 h-screen w-full bg-gr">
       <img src={home} alt='banner' className='h-88 md:h-full w-full'/>
       <img src={award} alt='award' className='h-88 md:hidden pt-3 w-full'/>
-      <span className='flex justify-center bg-orange-500 rounded-full py-1 px-4 mt-4 mx-auto sm:hidden'>
-        <Link to='gallery'>
+      <span className='justify-center bg-gradient-to-r from-orange-400 to-green-600 rounded-full py-1 px-4 mt-4 mx-auto sm:hidden'>
+        <Link to='gallery' className='flex'>
           Gallery
+          <BsArrowDownShort size={30} className='animate-bounce text-blue-800' />
         </Link>
-        <BsArrowDownShort size={30} className='animate-bounce text-blue-800' />
       </span>
     </div>
   )
