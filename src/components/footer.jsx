8191deeff0bfb65d2f4e1b66200bf396595c0f24
relish <guptarelish333@gmail.com>
2023-05-24 09:54:56 +0530

    finalized navbar and footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,63 +1,64 @@
-import {FaLinkedin, FaUser } from "react-icons/fa";
-import { HiOutlineMail,HiLocationMarker } from "react-icons/hi";
-import {BsFillTelephoneFill} from "react-icons/bs";
+import { FaLinkedin, FaUser } from "react-icons/fa";
+import { HiLocationMarker } from "react-icons/hi";
+import { SiGmail } from "react-icons/si"
+import { BsFillTelephoneFill } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-scroll";
 
 const Footer = () => {
-  const navigate=useNavigate();
-  const ToGallery=()=>navigate('/gallery');
-  const ToMissions=()=>navigate('/missions');
-  const ToHome=()=>navigate('/');
+  const navigate = useNavigate();
+  const ToGallery = () => navigate('/gallery');
+  const ToMissions = () => navigate('/missions');
+  const ToHome = () => navigate('/');
 
   return (
     <>
       <div name='contact' className="w-full bg-mrn">
-        <div className="border-white border-b pb-11 pt-1 flex flex-col md:flex-row md:justify-evenly">
+        <div className="border-black border-b-4 pb-11 pt-1 flex flex-col lg:flex-row lg:justify-evenly">
           <span className="pt-1 pl-2 md:pl-0">
-            <p className="flex text-green-400 text-lg">
-              <HiLocationMarker size={26} className="animate-bounce"/>
-              <span className="border-b text-black">
-                Office at village Ainpur, Tehsil: Bhajwal, Sunderbani, District: Rajouri, J&K, India 
-              </span><br/>
+            <p className="flex text-sky-400 text-lg">
+              <HiLocationMarker size={26} className="" />
+              <span className="md:border-b border-black  text-white">
+                Office at village Ainpur, Tehsil: Bhajwal, Sunderbani, District: Rajouri, J&K, India
+              </span><br />
             </p>
             <p className="pt-2 flex text-lg">
-              <FaUser size={20} className="animate-bounce"/>
-              <span className="pl-1 border-b text-black">
+              <FaUser size={20} className="" />
+              <span className="pl-1 md:border-b border-black  text-white">
                 Chairman/President: Zorawar Singh Khalsa
               </span>
             </p>
           </span>
-          <span className="flex flex-col pl-6 md:pl-0">
-            <h1 className="md:border-b pt-4 md:pt-1 text-xl text-black">Quick links</h1>
-            <p onClick={ToGallery} className="cursor-pointer text-lg text-yellow-400">Gallery</p>
-            <Link onClick={ToHome} to='about' className="cursor-pointer text-lg text-gray-300">About</Link>
-            <Link to='missions' smooth duration={500} onClick={ToMissions} className="cursor-pointer text-lg text-gray-300">Missions</Link>
-            <Link onClick={ToHome} to='projects' className="cursor-pointer text-lg text-gray-300">Projects</Link>
+          <span className="flex flex-col pl-7 md:pl-0">
+            <h1 className="md:border-b border-black pt-8 md:pt-1 text-xl text-black">Quick links</h1>
+            <p onClick={ToGallery} className="cursor-pointer text-lg text-white">Gallery</p>
+            <Link onClick={ToHome} to='about' className="cursor-pointer text-lg text-white">About</Link>
+            <Link to='missions' smooth duration={500} onClick={ToMissions} className="cursor-pointer text-lg text-white">Mission</Link>
+            <Link onClick={ToHome} to='projects' className="cursor-pointer text-lg text-white">Services</Link>
           </span>
-          <span className="text-center md:text-start -mb-9">
-            <p className="flex justify-center md:justify-start pt-2 text-md md:text-lg text-black">
-              <BsFillTelephoneFill className="animate-bounce" size={20} />
-              <span className="md:border-b pl-1 text-md md:text-lg">
+          <span className="mt-8 md:mt-0 text-center md:text-start -mb-9">
+            <p className="flex justify-center md:justify-start pt-2 text-md md:text-lg">
+              <BsFillTelephoneFill className=" text-green-400" size={20} />
+              <span className="md:border-b border-black  pl-1 text-md md:text-lg text-white">
                 9419131723, 9622360431
               </span>
             </p>
-            <a href="mailto:mrzsingh43Gmail.com" className="flex justify-center md:justify-start text-lg text-black" target="_blank" rel="noreferrer">
-              <HiOutlineMail size={20} className='text-green-400 animate-bounce' />
-              <span className="md:border-b pl-1 text-md md:text-lg hover:text-white">
+            <a href="mailto:mrzsingh43Gmail.com" className="flex justify-center md:justify-start text-lg text-white" target="_blank" rel="noreferrer">
+              <SiGmail size={20} className='text-green-400' />
+              <span className="md:border-b border-black lack pl-1 text-md md:text-lg hover:text-black">
                 mrzsingh43@gmail.com
               </span>
             </a>
           </span>
         </div>
-        <div className="mt-2 flex flex-col items-center md:flex-row md:justify-evenly text-black text-md md:text-lg">
+        <div className="flex flex-col items-center md:flex-row md:justify-evenly text-white text-md md:text-lg bg-red-800">
           <p>©2023 anmolculturalclub. All Rights Reserved</p>
           <p className="flex items-center">
             Designed by relishgupta
             <a href='https://www.linkedin.com/in/relish-gupta-165692234/' target="_blank" rel="noreferrer">
-              <FaLinkedin size={20} className='mx-2 text-sky-400 animate-bounce' />
+              <FaLinkedin size={20} className='mx-2 text-sky-500' />
             </a>
-            <a href="mailto:guptarelish333Gmail.com" target="_blank" rel="noreferrer"><HiOutlineMail size={20} className='animate-bounce text-green-400' /></a>
+            <a href="mailto:guptarelish333Gmail.com" target="_blank" rel="noreferrer"><SiGmail size={20} className=' text-green-400' /></a>
           </p>
         </div>
       </div>
--- src/routes/navbar.jsx
+++ src/routes/navbar.jsx
@@ -40,8 +40,24 @@ const Navbar = () => {
     return (
         <>
             <nav className='h-screen bg-cover bg-center' style={{ backgroundImage: `url(${home1})` }}>
+                {navb && (
+                    <>
+                        <div onClick={()=>{setNavb(false)}} className='fixed left-0 top-0 h-full w-full'/>
+                        <ul className='fixed z-30 w-[27rem] bg-gray-400 text-blue-800'>
+                        <FaTimes size={30} className="absolute right-8 mt-8 text-black cursor-pointer" onClick={()=>{setNavb(false)}}/>
+                        <img src={logo} alt='logo' className='h-20 ml-6 mt-2 cursor-pointer' onClick={Navigate} />
+                        {li.map(({ id, link }) => (
+                        <li key={id} className="border-t border-black px-10 cursor-pointer capitalize py-5 text-2xl hover:underline hover:text-green-800">
+                            <Link to={link}>
+                                {link}
+                            </Link>
+                        </li>
+                        ))}
+                        </ul>
+                    </>
+                )}
                 <div className="p-2 flex justify-center ">
-                    <div className='fixed bg-gray-400 rounded-full w-full xl:w-[76rem] flex justify-between items-center z-10'>
+                    <div className='fixed bg-gray-400 rounded-full w-80 md:w-full xl:w-[76rem] flex justify-between items-center z-10'>
                         <Link to='home' smooth duration={500}>
                             <img src={logo} alt='logo' className='h-20 ml-6 cursor-pointer' onClick={Navigate} />
                         </Link>
@@ -54,27 +70,12 @@ const Navbar = () => {
                             ))}
                         </ul>
 
-                        <div onClick={() => setNavb(!navb)} className='absolute right-10 cursor-pointer text-black md:hidden z-100'>
-                            {navb ? <FaTimes size={30} /> : <FaBars size={30} />}
+                        <div onClick={() => setNavb(true)} className='absolute right-10 cursor-pointer text-black md:hidden z-100'>
+                            <FaBars size={30} />
                         </div>
 
                     </div>
                 </div>
-                        {navb && (
-                            <>
-                            <div onClick={()=>{setNavb(false)}} className='fixed left-0 top-0 h-full w-full'/>
-                            <ul className='ml-5 rounded-lg fixed z-20 w-58 bg-gray-400 text-blue-800 '>
-                            <img src={logo} alt='logo' className='border-b-4 h-20 ml-6 cursor-pointer' onClick={Navigate} />
-                            {li.map(({ id, link }) => (
-                                    <li key={id} className="px-10 cursor-pointer capitalize py-5 text-2xl hover:underline hover:text-green-800">
-                                        <Link to={link} smooth duration={500}>
-                                            {link}
-                                        </Link>
-                                    </li>
-                            ))}
-                            </ul>
-                            </>
-                        )}
             </nav>
             <Outlet />
         </>
